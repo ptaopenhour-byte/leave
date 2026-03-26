d150b24f3aad4b1468fe7711deb10483d03df3d5
ptaopenhour-byte <ptaopenhour@ibsh.tw>
2026-03-26 16:04:12 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+window.FIREBASE_CONFIG = window.FIREBASE_CONFIG || {
+    apiKey: "AIzaSyDOoAP5fNTGDHtqPSS8jWWoNMyvLTqk1B4",
+    authDomain: "tardy-and-leave.firebaseapp.com",
+    projectId: "tardy-and-leave",
+    storageBucket: "tardy-and-leave.firebasestorage.app",
+    messagingSenderId: "940807032024",
+    appId: "1:940807032024:web:17f588917d4f30744d9bad",
+    measurementId: "G-BBERTFPHLN",
+};
+
+window.FIREBASE_OPTIONS = window.FIREBASE_OPTIONS || {
+    useEmulator: false,
+    emulatorHost: "127.0.0.1",
+    emulatorPort: 8080,
+};
